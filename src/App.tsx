d158aefe52e6cbab33459d07ce1aa89b46c3f37b
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-08-27 12:37:51 -0300

    refactor: manter apenas telas essenciais do CVTE

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,9 @@ import { DashboardView } from './views/DashboardView';
 import { CertificatesView } from './views/CertificatesView';
 import { CreateCertificateView } from './views/CreateCertificateView';
 import { BatchEmissionView } from './views/BatchEmissionView';
-import { CoursesView } from './views/CoursesView';
 import { StudentsView } from './views/StudentsView';
 import { ValidateCertificateView } from './views/ValidateCertificateView';
 import { SettingsView } from './views/SettingsView';
-import { ApiManagementView } from './views/ApiManagementView';
-import { AuditView } from './views/AuditView';
-import { BackupView } from './views/BackupView';
-import { ReportsView } from './views/ReportsView';
 
 const DATA_RESET_MARKER = 'certifyai_real_data_reset_v1';
 const CVTE_SWITCH_MARKER = 'certifyai_cvte_switch_v1';
@@ -25,13 +20,8 @@ const MainRouter: React.FC = () => {
       case 'certificates': return <CertificatesView />;
       case 'create-certificate': return <CreateCertificateView />;
       case 'batch-emission': return <BatchEmissionView />;
-      case 'courses': return <CoursesView />;
       case 'students': return <StudentsView />;
       case 'validate': return <ValidateCertificateView />;
-      case 'api': return <ApiManagementView />;
-      case 'audit': return <AuditView />;
-      case 'backup': return <BackupView />;
-      case 'reports': return <ReportsView />;
       case 'settings':
       case 'templates': return <SettingsView />;
       default: return <DashboardView />;
